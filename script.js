4a704207db307296103df559cafe892cf9c36d77
ashkdaywalker <116715716+ashkdaywalker@users.noreply.github.com>
2023-02-22 01:21:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,27 +5,22 @@ document.body.appendChild(getSumBtn);
 const getSum = () => {
 //Add your code here
 let a = document.getElementsByClassName("price");
-let b = 0;
+  let b = 0;
 
-let z = Array.from(a);
-z.forEach(function (item) {
-  b += parseInt(item.innerHTML);
-});
+  let z = Array.from(a);
+  z.forEach(function (item) {
+    b += parseInt(item.innerHTML);
+  });
 
-//   for (let i = 0; i < a.length; i++) {
-//     b += parseInt(a[i].innerHTML);
-//   }
-document.querySelector("table").innerHTML += `
-  <tr id="ans">
-    <td>${b}</td>
-  </tr>
-`;
-getSumBtn.disabled = true;
+  //   for (let i = 0; i < a.length; i++) {
+  //     b += parseInt(a[i].innerHTML);
+  //   }
+  document.querySelector("table").innerHTML += `
+    <tr id="ans">
+      <td>${b}</td>
+    </tr>
+  `;
+  getSumBtn.disabled = true;
 };
 
 getSumBtn.addEventListener("click", getSum);
-
-};
-
-getSumBtn.addEventListener("click", getSum);
-
